96042c14f58f7ca8b56a65616a0b9d13b9e9ac54
Rohit <rohitparih@gmail.com>
2026-02-10 16:48:31 +0530

    dsa -> `CountOddNumber`

---
+++ dsa/arrays/easy/CountOddNumber.java
@@ -0,0 +1,25 @@
+package dsa.arrays.easy;
+
+/*
+  Developer: Rohit Parihar
+  Project: InterviewPrep
+  GitHub: github.com/rohit-zip
+  File: CountOddNumber
+ */
+
+public class CountOddNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,6,8};
+        int oddNumbers = countOdd(arr);
+        System.out.println(oddNumbers);
+    }
+
+    private static int countOdd(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) count++;
+        }
+        return count;
+    }
+}
