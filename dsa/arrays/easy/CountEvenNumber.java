58bab33134cb712f9695d673848cd3d43da7d44d
Rohit <rohitparih@gmail.com>
2026-02-10 16:45:54 +0530

    dsa -> `CountEvenNumber`

---
+++ dsa/arrays/easy/CountEvenNumber.java
@@ -0,0 +1,27 @@
+package dsa.arrays.easy;
+
+/*
+  Developer: Rohit Parihar
+  Project: InterviewPrep
+  GitHub: github.com/rohit-zip
+  File: CountEvenNumber
+ */
+
+public class CountEvenNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,7};
+        int evens = countEven(arr);
+        System.out.println(evens);
+    }
+
+    static int countEven(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
