c7b5fdc914fb6eb7336d11996cf92ec01f902165
Rohit <rohitparih@gmail.com>
2026-02-10 17:24:40 +0530

    dsa -> `CountTotalElementGreaterThanGiven`

---
+++ dsa/arrays/easy/CountTotalElementGreaterThanGiven.java
@@ -0,0 +1,25 @@
+package dsa.arrays.easy;
+
+/*
+  Developer: Rohit Parihar
+  Project: InterviewPrep
+  GitHub: github.com/rohit-zip
+  File: CountTotalElementGreaterThanGiven
+ */
+
+public class CountTotalElementGreaterThanGiven {
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,8,3};
+        int count = countElements(arr, 4);
+        System.out.println(count);
+    }
+
+    private static int countElements(int[] arr, int x) {
+        int count = 0;
+        for (int num : arr) {
+            if (x > num) count++;
+        }
+        return count;
+    }
+}
