9f10e8de36f9118acb98e1a7c54ad3e05bb0e404
Rohit <rohitparih@gmail.com>
2026-02-10 14:16:27 +0530

    dsa -> `FindMaximumElement`

---
+++ dsa/arrays/easy/FindMaximumElement.java
@@ -0,0 +1,27 @@
+package dsa.arrays.easy;
+
+/*
+  Developer: Rohit Parihar
+  Project: InterviewPrep
+  GitHub: github.com/rohit-zip
+  File: FindMaximumElement
+ */
+
+public class FindMaximumElement {
+
+    public static void main(String[] args) {
+        int[] arr = {3,7,1,9,4};
+        int largest = find(arr);
+        System.out.println(largest);
+    }
+
+    static int find(int[] arr) {
+        int max = arr[0];
+        for (int i = 0 ; i < arr.length ; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
