96af914bb8ca44a71c333afd7e472c6841e0503c
Rohit <rohitparih@gmail.com>
2026-02-11 17:03:20 +0530

    dsa -> `BuyAndSellStock`

---
+++ dsa/arrays/medium/BuyAndSellStock.java
@@ -0,0 +1,49 @@
+package dsa.arrays.medium;
+
+/*
+  Developer: Rohit Parihar
+  Project: InterviewPrep
+  GitHub: github.com/rohit-zip
+  File: BuyAndSellStock
+ */
+
+public class BuyAndSellStock {
+
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+        int profit = bruteForce(arr);
+        System.out.println(profit);
+    }
+
+    static int bruteForce(int[] arr) {
+        int max = 0;
+        for (int i = 0 ; i < arr.length - 1 ; i++) {
+            for (int j = i + 1 ; j < arr.length ; j++) {
+                int profit = arr[j] - arr[i];
+                if (profit > max) {
+                    max = profit;
+                }
+            }
+        }
+        return max;
+    }
+
+    static int greedyApproach(int[] arr) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0 ; i < arr.length ; i++) {
+            if (arr[i] < minPrice) {
+                minPrice = arr[i];
+            }
+
+            int profit = arr[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
